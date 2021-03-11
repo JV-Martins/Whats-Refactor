9aa500e85422fc89080433364bf8a8c43762219c
felipewesley <fe.wesleybasso@gmail.com>
2021-03-10 21:06:18 -0300

    melhoria layout da tela conversas e navegacao

---
+++ js/index-script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+
+    let btn_nova_conversa = document.getElementById("nova-conversa");
+    btn_nova_conversa.onclick = function() {
+        window.location.href = "contatos.html";
+    }
+}
